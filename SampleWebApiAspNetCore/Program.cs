c37ad2e7ed01dde0a219e127be607e457637d00d
subodh <subudear@yahoo.com>
2020-02-16 21:27:08 +1300

    first commit

---
+++ SampleWebApiAspNetCore/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SampleWebApiAspNetCore.Repositories;
+using SampleWebApiAspNetCore.Services;
+using System;
+
+namespace WebApplication11
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            // Initializes db.
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<FoodDbContext>();
+                    var dbInitializer = services.GetRequiredService<ISeedDataService>();
+                    dbInitializer.Initialize(context).GetAwaiter().GetResult();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ SampleWebApiAspNetCore/Services/SeedDataService.cs
@@ -0,0 +1,20 @@
+﻿using SampleWebApiAspNetCore.Entities;
+using SampleWebApiAspNetCore.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace SampleWebApiAspNetCore.Services
+{
+    public class SeedDataService : ISeedDataService
+    {
+        public async Task Initialize(FoodDbContext context)
+        {
+            context.FoodItems.Add(new FoodItem() { Calories = 1000, Name = "Lasagne", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodItem() { Calories = 1100, Name = "Hamburger", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodItem() { Calories = 1200, Name = "Spaghetti", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodItem() { Calories = 1300, Name = "Pizza", Created = DateTime.Now });
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
